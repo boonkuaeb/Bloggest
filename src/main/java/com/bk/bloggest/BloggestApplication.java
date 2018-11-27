c38347cf1910fe601d9097af4e28a4e13060a116
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-27 12:14:57 +0700

    first commit

---
+++ src/main/java/com/bk/bloggest/BloggestApplication.java
@@ -0,0 +1,12 @@
+package com.bk.bloggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggestApplication.class, args);
+    }
+}
